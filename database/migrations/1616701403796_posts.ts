b21750f363ff19de222b61c4db961ac97c3f0641
4scencio <alessandroascencio@protonmail.com>
2021-03-25 16:55:57 -0300

    create column unique (migration)

--- database/migrations/1616701403796_posts.ts
+++ database/migrations/1616701403796_posts.ts
@@ -6,7 +6,7 @@ export default class Posts extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('title')
+      table.string('title').unique()
       table.text('content', 'longtext' )
       table.timestamps(true)
     })
