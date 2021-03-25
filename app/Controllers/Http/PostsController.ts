ba24a9f211873131ce0be22ed420ebfc19eb1575
4scencio <alessandroascencio@protonmail.com>
2021-03-25 16:06:05 -0300

    Crud Controller Posts

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,7 +1,37 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
 
 export default class PostsController {
-    index () {
-        return ['post 1', 'post 2', 'post 3']
-    }
+  public async index ({}: HttpContextContract) {
+    const posts = await Post.all()
+
+    return posts
+  }
+
+  public async store ({ request }: HttpContextContract) {
+    const data = request.only(['title', 'content'])
+    const post = await Post.create(data)
+
+    return post
+  }
+
+  public async show ({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+
+    return post
+  }
+
+  public async update ({ params, request }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    const data = request.only(['title', 'content'])
+
+    post.merge(data)
+    await post.save()
+
+    return post
+
+  }
+
+  public async destroy ({}: HttpContextContract) {
+  }
 }
